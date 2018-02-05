d8a9983bcdbdbe2f2be5eea3a86419075fa3dcd3
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-02-05 18:40:59 +0100

    terminado index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php defined( 'JEXEC') or die('RESTRICTED ACCES');?>
 <!DOCTYPE html>
-	<html xmlns="http://www.w3.org/xhtml" xml:lang="<?php"
+	<html xmlns="http://www.w3.org/xhtml" xml:lang="<?php echo $this->languaje; ?>">
+<head>
+<jdoc:include type="head" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>css/plantilla.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>css/plantilla.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>css/plantilla.css/<?php echo $this->template; ?>css/plantilla.css" type="text/css" />
+</head>
+<body>
+<jdoc:include type="modules" name="top" /> 
+<jdoc:include type="component" />
+<jdoc:include type="modules" name="bottom" />
+</body>
+</html>
