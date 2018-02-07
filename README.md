45f9764d97ef8acee1ecbc08c687c70b2ae2a686
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-02-07 10:37:17 +0100

    Terminada primera parte

--- README.md
+++
@@ -1,2 +0,0 @@
-# plantillajoomla
-En este repositorio subiré las plantilla de joomla que se hagan como ejercicio de clase
---
+++ plantilla/index.php
@@ -0,0 +1,31 @@
+<?php defined( '_JEXEC') or die( 'Restricted access' );?>
+<!doctype html>
+<html     class="no-js"
+          xmlns="http://www.w3.org/1996/xhtml"
+          xml:lang="<?php echo $this->language; ?>"
+          lang="<?php echo $this->language; ?>"
+          
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+
+        <p>plantilla furuladora.</p>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>windows.jquery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
